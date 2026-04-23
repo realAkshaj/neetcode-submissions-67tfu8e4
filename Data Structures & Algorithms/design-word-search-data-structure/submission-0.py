da6d892d16d867defac41767361cb28047bdf72a
Akshaj <156048642+realAkshaj@users.noreply.github.com>
2026-04-23 17:05:20 -0500

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,41 @@
+class Node:
+
+    def __init__(self):
+
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()        
+
+    def addWord(self, word: str) -> None:
+
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node()
+            curr = curr.children[c]
+        curr.word = True
+        
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j,root):
+            curr = root  
+            for i in range(j,len(word)):
+                if word[i] == '.':
+                    for child in curr.children.values():
+                        if dfs(i+1,child):
+                            return True
+                    return False                
+                else:
+                    if word[i] not in curr.children:
+                        return False
+                    curr = curr.children[word[i]]
+            return curr.word
+        
+        return dfs(0,self.root)
+            
+        
